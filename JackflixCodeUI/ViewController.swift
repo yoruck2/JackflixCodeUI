3459af555de94c618e3d20975c33801cb7266c1d
yoruck2 <yoruck2@gmail.com>
2024-06-05 01:26:01 +0900

    Initial Commit

---
+++ JackflixCodeUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JackflixCodeUI
+//
+//  Created by dopamint on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
